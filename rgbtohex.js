b4d0ee9cb15e30895d70a0faaa75fc3663099737
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-04 19:40:49 +0100

    Update rgbtohex.js

--- rgbtohex.js
+++ rgbtohex.js
@@ -3,20 +3,12 @@ var rgb={
  tohex:function(s){
   var f,r,g,b,m=s.match(this.rgx);
   if(m!==null){
-	m=m.slice(1);
-	m[0]='#';
-	r=m[1];g=m[2];b=m[3];
+	m=m.slice(1);m[0]='#';r=m[1];g=m[2];b=m[3];
 	f=function(s){return('0'+parseInt(s).toString(16)).slice(-2);};
-	if(r===g && g===b){
-		r=f(r);g=r.charAt(1);b=r.charAt(2);
-	}else{
-		r=f(r);g=f(g);b=f(b);
-	};
+	if(r===g && g===b){r=f(r);g=r.charAt(1);b=r.charAt(2);}else{r=f(r);g=f(g);b=f(b);};
 	m[1]=r;m[2]=g;m[3]=b;
 	return m.join('');
   };
  }
 };
-//
-console.log(rgb.tohex('rgb( 0, 0, 0)'));
-
+//console.log(rgb.tohex('rgb( 0, 0, 0)'));
