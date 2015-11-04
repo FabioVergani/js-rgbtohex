bad683db14ea5b2027fe913ac4ac99550b5e0de9
Fabio Vergani <fabio.vergani.1975@gmail.com>
2015-11-04 20:58:36 +0100

    Create matches2hex.js

---
+++ matches2hex.js
@@ -0,0 +1,28 @@
+window.log=console.log.bind(console);
+//=========================================
+
+var rgb={
+ rgx:/rgb(a?)\s*\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3}),?\s*(0?\.\d+|[01])?\s*\)/i,
+ matches2hex:function(a){
+	var m=a,f=',';
+	if(m[1]==='a'){
+		m=m.slice(1,6);
+		m[0]='rgba(';
+		m[1]+=f;
+		m[2]+=f;
+		m[3]+=f;
+		f=Number(m[4]);
+		m[4]=String(f===0||f===1?f:f.toFixed(2));
+		m[5]=')';
+	}else{
+	 f=function(s){return('0'+parseInt(s).toString(16)).slice(-2);};
+	 m=a.slice(1,5),r=m[1],g=m[2],b=m[3],v=m[1]=f(r);
+	 m[0]='#';
+	 if(r===g && g===b){g=v.charAt(1);b=v.charAt(2);}else{g=f(g);b=f(b);};
+	 m[2]=g;
+	 m[3]=b;
+	};
+	return m.join('');
+ }
+};
+//console.log(rgb.tohex('rgb( 0, 0, 0)'));
